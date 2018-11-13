8a79a3503bee2c242effcad3a822f4aa99d9ec09
Prakat Tuladhar <prakat.tuladhar@bluecrossmn.com>
2018-11-13 12:57:46 -0600

    new buid

--- appcenter-pre-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo ===========
-echo "moving patched react.gradle file into node_modules/react-native..."
-mv $APPCENTER_SOURCE_DIRECTORY/android/react.gradle.mv $APPCENTER_SOURCE_DIRECTORY/node_modules/react-native/react.gradle
