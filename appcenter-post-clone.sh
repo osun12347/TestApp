7f00121b4212e2c21a2d3d19897840545382aaa2
Prakat Tuladhar <prakat.tuladhar@bluecrossmn.com>
2018-11-13 12:43:54 -0600

    hope it works

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,13 +1,7 @@
+
 #!/usr/bin/env bash
-# https://intercom.help/appcenter/build/react-native-builds-fail-with-the-engine-node-is-incompatible-with-this-module-expected-version-xxx-error-found-incompatible-module
 set -ex
-brew uninstall node -f
+brew uninstall node@6
 NODE_VERSION="10.5.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-
-
-echo "Copy in correct .env file based on AppCenter RELEASE_ENV variable"
-cp $APPCENTER_SOURCE_DIRECTORY/env/$RELEASE_ENV.txt $APPCENTER_SOURCE_DIRECTORY/.env
-
-brew install awscli
