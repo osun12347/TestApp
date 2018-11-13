f5bf068971f9f6448c068a668cc4d1fde9cc790d
Prakat Tuladhar <prakat.tuladhar@bluecrossmn.com>
2018-11-13 12:50:26 -0600

    tst

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,10 @@
 
 #!/usr/bin/env bash
+echo "post clone was executer"
 set -ex
 brew uninstall node@6
 NODE_VERSION="10.5.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+
+echo "post clone was executer"
