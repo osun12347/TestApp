93192504bc6162b8af8c95b4080b4e2549050935
Prakat Tuladhar <prakat.tuladhar@bluecrossmn.com>
2018-11-14 11:17:04 -0600

    gradle

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,5 +5,7 @@ brew uninstall node@6
 NODE_VERSION="10.5.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-
+./andorid/gradlew build
+cd andorid
+./gradlew tasks
 
