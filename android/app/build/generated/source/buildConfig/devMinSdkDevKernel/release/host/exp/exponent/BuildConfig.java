fbb8b06ee56bbb6e96b3ea854421828baeb06bda
Prakat Tuladhar <prakat.tuladhar@bluecrossmn.com>
2018-11-14 13:21:07 -0600

    build variant

---
+++ android/app/build/generated/source/buildConfig/devMinSdkDevKernel/release/host/exp/exponent/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package host.exp.exponent;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "android.Bundler";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "devMinSdkDevKernel";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  public static final String FLAVOR_minSdk = "devMinSdk";
+  public static final String FLAVOR_remoteKernel = "devKernel";
+}
